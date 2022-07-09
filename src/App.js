64b20aceba36e5058eb9fbe9dd162bdbb3a3765d
bully82 <goladzo@outlook.com>
2022-07-09 10:50:12 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import { NavBar } from "./ui-components";
 import { SideBar } from "./ui-components";
 import './fonts/F1.8-Regular.otf';
-
+import ActionCard from './ui-components/ActionCard';
 function App() {
   return (
     <div>
-      
-      <NavBar width='100%'/>
-      <SideBar fontFamily='sans-serif'/>
-<p>texterooony soccer soccer</p>
+          <NavBar/>
+      <ActionCard position='centre'/>
+      <p>texterooony soccer soccer</p>
+      <SideBar/>
     </div>
   );
 }
 
 export default withAuthenticator(App,{ includegreeting:true});
-
 
